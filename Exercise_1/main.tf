e057fe7026a86f328cc7cd79e5cbc115d5e2b9d1
Trường Huỳnh <25319326+huynhphamduytruong@users.noreply.github.com>
2022-09-04 02:14:29 +0700

    Add Tag name

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,23 +1,31 @@
-# TODO: Designate a cloud provider, region, and credentials
+# Designate a cloud provider, region, and credentials
 provider "aws" {
   region  = "us-east-1" 
   profile = "cloud_architect"
 }
 
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+# provision 4 AWS t2.micro EC2 instances named Udacity T2
 resource "aws_instance" "Udacity-T2" {
   ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0f5742c5494627a2c"
   count         = 4
+
+  tags = {
+    Name = "Udacity T2"
+  }
 }
 
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+# provision 2 m4.large EC2 instances named Udacity M4
 resource "aws_instance" "Udacity-M4" {
   ami           = "ami-05fa00d4c63e32376"
   instance_type = "m4.large"
   subnet_id     = "subnet-0f5742c5494627a2c"
   count         = 2
+
+  tags = {
+    Name = "Udacity M4"
+  }
 }
